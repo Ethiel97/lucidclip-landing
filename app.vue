1441d186dc9a4ec7df41478823e170d11ae3b629
Ethiel <ethiel97@gmail.com>
2026-05-08 06:20:16 -0400

    feat(landing): update Open Graph and Twitter image URLs to use new image

--- app.vue
+++ app.vue
@@ -19,9 +19,9 @@ useSeoMeta({
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://lucidclip.app/images/og.png',
-  ogImageUrl: 'https://lucidclip.app/images/og.png',
-  ogImageSecureUrl: 'https://lucidclip.app/images/og.png',
+  ogImage: 'https://lucidclip.app/images/og-image.png',
+  ogImageUrl: 'https://lucidclip.app/images/og-image.png',
+  ogImageSecureUrl: 'https://lucidclip.app/images/og-image.png',
   ogType: 'website',
   ogUrl: 'https://lucidclip.app',
   twitterTitle: title,
@@ -29,7 +29,7 @@ useSeoMeta({
   twitterSite: '@lucidclipapp',
   twitterCreator: '@lucidclipapp',
   ogSiteName: 'LucidClip',
-  twitterImage: 'https://lucidclip.app/images/og.png',
+  twitterImage: 'https://lucidclip.app/images/og-image.png',
   twitterCard: 'summary_large_image',
 })
 </script>
